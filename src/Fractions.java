c8b7769f35904f6016e960860682e2e9ba86f326
bistenofficial <maks.kirov.2002@mail.ru>
2021-04-29 11:15:15 +0300

    Edit Fraction class

--- src/Fractions.java
+++ src/Fractions.java
@@ -3,19 +3,27 @@ public class Fractions {
     private int _wholeNum;
     private int _numberDigitsFractionalNumPoint;
     private int _fractionalNum;
+    public Fractions(String num) {
+        String[] arr2 = new String[2];
+        String[] arr = num.split("\\.");
+        if (arr.length == 2) {
+            _numberDigitsFractionalNumPoint = arr[1].length();
+            SetFractionalNum(Integer.valueOf(arr[1]));
+            SetWholeNum(Integer.valueOf(arr[0]));
+        } else {
+            _numberDigitsFractionalNumPoint = 0;
+            SetFractionalNum(0);
+            SetWholeNum(Integer.valueOf(arr[0]));
+        }
 
-    public Fractions(double num) {
-
-        String[] arr =  String.valueOf(num).split("\\.");
-        _numberDigitsFractionalNumPoint = arr[1].length();
-        SetFractionalNum(Integer.valueOf(arr[1]));
-        SetWholeNum(Integer.valueOf(arr[0]));
 
     }
 
-    public void SetFractionalNum(int fractionalNum) {
+    public void SetFractionalNum(int fractionalNum) throws IllegalArgumentException {
         if (fractionalNum >= 0) {
             this._fractionalNum = fractionalNum;
+        } else {
+            throw new IllegalArgumentException("Ввели отрицательную дробь");
         }
     }
 
@@ -23,29 +31,29 @@ public class Fractions {
         this._wholeNum = wholeNum;
     }
 
-    public int GetWholeNum(){
+    public int GetWholeNum() {
         return _wholeNum;
     }
 
-    public int GetFractionalNum(){
+    public int GetFractionalNum() {
         return _fractionalNum;
     }
 
-    public int GetNumberDigitsFractionalNumPoint(){
+    public int GetNumberDigitsFractionalNumPoint() {
         return _numberDigitsFractionalNumPoint;
     }
 
-    private int GetSign(int num){
-        if (num>=0){
+    private int GetSign(int num) {
+        if (num >= 0) {
             return 1;
-        }else {
+        } else {
             return -1;
         }
     }
 
-    public double Summ(Fractions fractions){
-        return _wholeNum+_fractionalNum/Math.pow(10,_numberDigitsFractionalNumPoint)* GetSign(_wholeNum) + fractions.GetWholeNum() +
-                fractions.GetFractionalNum()/Math.pow(10,fractions.GetNumberDigitsFractionalNumPoint())*GetSign(fractions.GetWholeNum());
+    public double Summ(Fractions fractions) {
+        return _wholeNum + _fractionalNum / Math.pow(10, _numberDigitsFractionalNumPoint) * GetSign(_wholeNum) + fractions.GetWholeNum() +
+                fractions.GetFractionalNum() / Math.pow(10, fractions.GetNumberDigitsFractionalNumPoint()) * GetSign(fractions.GetWholeNum());
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
-
-
 public class Main {
 
     public static void main(String[] args) {
-
-        Fractions fractions = new Fractions(-19.06);
-
-        Fractions fractions2 = new Fractions(0.002);
-
+        Fractions fractions = new Fractions("-19.9");
+        Fractions fractions2 = new Fractions("-2.11");
         System.out.println(fractions.Summ(fractions2));
     }
 }
